2791717eb37e9f82af9bfb11d63fd2e644a57be3
SeaswimmerTheFsh <102361830+SeaswimmerTheFsh@users.noreply.github.com>
2023-03-23 14:43:16 -0400

    a

--- bot.py
+++ bot.py
@@ -1,28 +1,55 @@
 import os
 import discord
+from discord import app_commands
 from discord.ext import commands
+from discord.ext.commands import Greedy, Context
 from dotenv import load_dotenv
 import time
 
 load_dotenv()
 token = os.getenv('TOKEN')
 
-intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds=True
+intents.members=True
+intents.reactions=True
 
 bot = commands.Bot(command_prefix='-', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged on as {bot.user}!')
+    print(f"""Logged on as {bot.user}!""")
 
-@bot.command(pass_context=True)
-async def ping(ctx):
-    """ Pong! """
-    await ctx.message.delete()
+class Core(discord.Client):
+  def __init__(self):
+    super().__init__(intents = intents)
+    self.synced = False
+    self.added = False
+
+async def on_ready(self):
+    await self.wait_until_ready()
+    if not self.synced:
+        await tree.sync(guild = discord.Object('1088276267872296972'))
+        self.synced = True
+    if not self.added:
+        self.added = True
+
+client = Core()
+tree = discord.app_commands.CommandTree(client)
+
+@bot.event
+async def on_message(message):
+    if message.author != bot.user:
+        print(f"{message.author}: {message.content}")
+
+@tree.command(description="Checks the bot's latency.", guild=discord.Object('1088276267872296972'))
+async def ping(interaction: discord.Interaction) -> None:
+    """Checks the bot's latency."""
     before = time.monotonic()
-    message = await ctx.send("Pong!")
+    await interaction.response.send_message("Pong!", ephemeral=True)
     ping = (time.monotonic() - before) * 1000
-    await message.edit(content=f"Pong!  `{int(ping)}ms`")
+    await discord.InteractionMessage.edit(content=f"Pong!  `{int(ping)}ms`")
     print(f'Ping {int(ping)}ms')
 
 @bot.command()
