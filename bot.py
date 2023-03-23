c1d321ecfc5564f90a4ba80ea119cda572c63ddd
SeaswimmerTheFsh <102361830+SeaswimmerTheFsh@users.noreply.github.com>
2023-03-23 11:27:18 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,26 +7,39 @@ import time
 load_dotenv()
 token = os.getenv('TOKEN')
 
-intents = discord.Intents(messages=True, guilds=True)
+intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)
 
 bot = commands.Bot(command_prefix='-', intents=intents)
 
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user}!')
 
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-    
-    @bot.command(pass_context=True)
-    async def ping(ctx):
-        """Checks bot latency. """
-        await ctx.delete_message()
-        before = time.monotonic()
-        message = await ctx.send("Pong!")
-        ping = (time.monotonic() - before) * 1000
-        await message.edit(content=f"Pong!  `{int(ping)}ms`")
-        print(f'Ping {int(ping)}ms')
+@bot.command(pass_context=True)
+async def ping(ctx):
+    """ Pong! """
+    await ctx.message.delete()
+    before = time.monotonic()
+    message = await ctx.send("Pong!")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"Pong!  `{int(ping)}ms`")
+    print(f'Ping {int(ping)}ms')
 
-client = Client(intents=intents)
-client.run(token)
+@bot.command()
+async def dm(ctx: commands.Context, user: discord.Member, *, message: str):
+    """Sends a Direct Message to a user."""
+    try:
+        await user.send(message)
+    except (discord.HTTPException, discord.Forbidden) as error:
+        await ctx.send(content="That user has their direct messages closed!")
+    finally:
+        await ctx.message.add_reaction("✅")
+
+@dm.error
+async def dm_error(ctx, error):
+    """Error handling for DM command."""
+    if isinstance(error, commands.MemberNotFound):
+        await ctx.send(content="That is not a user or a user id!")
+        return
+
+bot.run(token)
