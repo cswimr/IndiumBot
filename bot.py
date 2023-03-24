9a6077d223dc5759247f8d2fa0873edebfaf7e2b
idm22k <videogamemusician123@gmail.com>
2023-03-23 20:45:42 -0400

    Added member to MessageModal

--- bot.py
+++ bot.py
@@ -34,6 +34,44 @@ class Core(discord.Client):
 client = Core()
 tree = discord.app_commands.CommandTree(client)
 
+class MessageModal(discord.ui.Modal, title="Message"):
+    def __init__(self, member):
+        super().__init__()
+        self.member = member
+
+    message = discord.ui.TextInput(
+        label="Message Content",
+        placeholder="I'm contacting you about your cars extended warranty...",
+        style=discord.TextStyle.paragraph,
+        max_length=2000
+    )
+    secondary_message = discord.ui.TextInput(
+        label="Secondary Message Content",
+        placeholder="Typically used for images/image links.",
+        style=discord.TextStyle.paragraph,
+        required=False,
+        max_length=2000
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        try:
+            await self.member.send(message)
+            if secondary_message:
+                await self.member.send(secondary_message)
+        except (discord.HTTPException, discord.Forbidden) as error:
+            await interaction.response.send_message(content="That user has their direct messages closed!",
+                                                    ephemeral=True)
+        finally:
+            await interaction.response.send_message(
+                content=f"Message sent!\nMessage contents:\n```{message}```",
+                ephemeral=True)
+
+@tree.command(description="Sends a direct message to a user.", guild=discord.Object('1088276267872296972'))
+@discord.app_commands.describe(member="What member are you sending a message to?")
+async def message(interaction: discord.Interaction, member: discord.Member):
+    """Sends a direct message to a user."""
+    await interaction.response.send_modal(MessageModal(member))
+
 @client.event
 async def on_message(message):
     if message.author != client.user:
@@ -48,16 +86,6 @@ async def ping(interaction: discord.Interaction):
     embed=discord.Embed(title="🏓 Pong!", description=f"```py\n{int(ping)} ms```", color=15844367)
     await interaction.edit_original_response(content=None, embed=embed)
     print(f'Ping {int(ping)}ms')
-
-@tree.command(description="Sends a direct message to a user.", guild=discord.Object('1088276267872296972'))
-@discord.app_commands.describe(member="What member are you sending a message to?", message="Input the message you're sending.")
-async def message(interaction: discord.Interaction, member: discord.User, message: str):
-   """Sends a direct message to a user."""
-   try:
-      await member.send(message)
-      await interaction.response.send_message(content=f"Message sent!\nMessage contents:\n```{message}```", ephemeral=True)
-   except (discord.HTTPException, discord.Forbidden) as error:
-      await interaction.response.send_message(content="That user has their direct messages closed or is not in the server!", ephemeral=True)
 
 @tree.command(description="Sends a message to a channel.", guild=discord.Object('1088276267872296972'))
 @discord.app_commands.describe(channel="What channel are you sending this message to?", message="Input the message you're sending.")
